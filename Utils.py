63982b08ac6c146aad4710e7e7f43e20e3089b60
AKNOUCHEanis <ga_aknouche@esi.dz>
2021-02-27 02:20:18 +0100

    MAJ 1.0 Classifieurs

---
+++ Utils.py
@@ -0,0 +1,107 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb 27 01:20:49 2021
+
+@author: DELL VOSTRO
+"""
+import nltk
+import numpy as np
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from sklearn.feature_extraction.text import CountVectorizer
+
+
+def getCVTextsAndLabels(data_cv):
+    """
+    Parameters
+    ----------
+    data_cv : BDD contenant tous les CVs, sous forme d'une liste de dictionnaires'
+
+    Returns
+    -------
+    textsCV : une liste de tous les textes des CVs
+    labelsCV : une liste de tous les labels des CVs (Domaine industriel)
+
+    """
+    labelsCV=[]
+    textsCV=[]
+    for nbCV in range(5000): 
+        textCVi=[]
+        for i in data_cv[nbCV].get('jobs'):
+            textCVi=textCVi+list(i.values())
+        textCVi=textCVi+data_cv[nbCV].get('skills')
+        for i in data_cv[nbCV].get('edu'):
+            textCVi=textCVi+list(i.values())
+        labelsCV.append(data_cv[nbCV].get('industry'))
+        textCVi=list(map(str, textCVi))
+        textsCV.append(' '.join(textCVi))
+    return textsCV, labelsCV
+
+
+
+def tokenize(textsCV):
+    """
+    Parameters
+    ----------
+    textsCV : la liste des textes de tous les CVs
+
+    Returns
+    -------
+    textCVFinal : la liste de textes de tous les CVs aprés la tokenization
+
+    """
+
+    stop_words = set(stopwords.words('french'))
+    
+    tokenizer = nltk.RegexpTokenizer(r"\w+")
+    textCVFinal=[]
+    for cv in textsCV:
+        words=tokenizer.tokenize(cv)
+        new_sentence=''
+        for word in words:
+            if word not in stop_words:
+                new_sentence=new_sentence+' '+word.upper()
+            
+        textCVFinal.append(new_sentence)
+        
+    return textCVFinal
+
+def vectorization(textsCV,labelsCV):
+    """
+    Parameters
+    ----------
+    textsCV : liste des textes des CVs
+    labelsCV : Liste des labels des CVs
+
+    Returns
+    -------
+    dataSet_x : une matrice où les lignes représentent les documents et les colonnes les mots
+    et contient la fréquence d'apparition de chaque mot dans le document correspondant'
+    dataLabel_y : un tableau des labels des CVs (Domaine industriel) numérisés
+    label_y : contient une liste de tous les domaines industriels
+
+    """
+    vectorizer=CountVectorizer()
+    vector=vectorizer.fit_transform(textsCV)
+    dataSet_x=vector.toarray()
+    dataLabel_y=np.array(labelsCV)
+    label_y=list(np.unique(dataLabel_y))
+    dataLabel_y_num=[label_y.index(y) for y in dataLabel_y]
+    return dataSet_x, dataLabel_y_num, label_y
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
--- main.py
+++ main.py
@@ -8,6 +8,8 @@ Created on Mon Feb  8 00:28:59 2021
 import pickle
 import numpy as np
 from Utils import getCVTextsAndLabels, tokenize, vectorization
+from sklearn import model_selection 
+from Classifieurs import Svm
 
 
 data_cv = pickle.load( open( "CV_5000_PLDAC.pkl", "rb" ) )
@@ -30,9 +32,17 @@ textsCV, labelsCV=getCVTextsAndLabels(data_cv)
 textsCV=tokenize(textsCV)
 dataSet_x, dataLabel_y, labels=vectorization(textsCV, labelsCV)
 
-print(np.shape(dataSet_x))
+#découpage des données d'entrainements et tests
+x_train, x_test, y_train, y_test = model_selection.train_test_split(dataSet_x,dataLabel_y, train_size=0.80, test_size=0.20, random_state=101)
 
+"""
+#Test d'un classifieur SVM
 
+svm=Svm(C=2,kernel='poly',degree=2)
+svm.fit(x_train, y_train)
 
+print('Accuracy (poly Kernel): ', "%.2f" % (svm.accuracy(svm.predict(x_test),y_test)*100))
+print('F1 (poly Kernel): ', "%.2f" % (svm.fMeasure(svm.predict(x_test),y_test)*100))
+"""
 
-
+#Test d'un classifieur KNN
